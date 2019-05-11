e4ff67cde9582ae5399a3eb2f883386de05bbf30
Kong Yang <kongyang@Kongs-MacBook-Air.local>
2019-05-11 19:11:20 -0400

    app work

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "burgers_db"
+if (process.env.JAWSDB_URL) {
+    connection = URL.createConnection(process.env.JAWSDB_URL);
+} else {
+    
+    var connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "root",
+        database: "burgers_db"
+    
+    });
+}
 
-});
 
 connection.connect(function (err) {
     if (err) throw err;
