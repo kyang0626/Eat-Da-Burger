eb317c2a87da82a9126249cda5c90cf2431aebdd
Kong Yang <kongyang@Kongs-MacBook-Air.local>
2019-05-12 01:23:03 -0400

    minor changes in css

--- config/connection.js
+++ config/connection.js
@@ -5,8 +5,8 @@ var connection;
 if (process.env.JAWSDB_URL) {
     connection = URL.createConnection(process.env.JAWSDB_URL);
 } else {
-    
-         connection = mysql.createConnection({
+
+        connection = mysql.createConnection({
         host: "localhost",
         port: 3306,
         user: "root",
@@ -15,7 +15,6 @@ if (process.env.JAWSDB_URL) {
     
     });
 }
-
 
 connection.connect(function (err) {
     if (err) throw err;
